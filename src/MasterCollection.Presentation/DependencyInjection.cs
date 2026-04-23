a828ac5d2a9857e0b50a9d983129b2d74835fc40
lquqpgqr2 <abir.cel299@gmail.com>
2026-04-23 13:25:02 +0600

    update persentation DependencyInjection.cs

--- src/MasterCollection.Presentation/DependencyInjection.cs
+++ src/MasterCollection.Presentation/DependencyInjection.cs
@@ -1,6 +1,17 @@
+using MasterCollection.Presentation.Extensions;
+
 namespace MasterCollection.Presentation;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddPresentation(IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.AddPresentationOptions(configuration);
+            services.AddCorsPolicy(configuration, environment);
+            
+            return services;
+        }
+    }
 }
